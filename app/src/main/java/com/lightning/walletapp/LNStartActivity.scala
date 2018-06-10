896918ffc7c08fda444f5ebfd62e982da060e384
anton <admin@btcontract.com>
2018-06-10 10:24:01 +0300

    Propose to a open a channel with payee when getting a payment request with no open channels
    # Conflicts:
    #	app/src/main/java/com/lightning/walletapp/FragWallet.scala
    #	app/src/main/java/com/lightning/walletapp/LNStartActivity.scala

--- app/src/main/java/com/lightning/walletapp/LNStartActivity.scala
+++ app/src/main/java/com/lightning/walletapp/LNStartActivity.scala
@@ -3,25 +3,23 @@ package com.lightning.walletapp
 import com.lightning.walletapp.ln._
 import com.lightning.walletapp.Utils._
 import com.lightning.walletapp.R.string._
-import com.lightning.walletapp.lnutils.olympus.OlympusWrap._
+import com.lightning.walletapp.StartNodeView._
 import com.lightning.walletapp.lnutils.ImplicitConversions._
-
-import android.view.{Menu, View, ViewGroup}
-import android.widget.{BaseAdapter, ListView, TextView}
+import com.lightning.walletapp.lnutils.olympus.OlympusWrap._
 import com.lightning.walletapp.ln.wire.NodeAnnouncement
 import com.lightning.walletapp.helper.ThrottledWork
 import com.lightning.walletapp.ln.Tools.runAnd
 import com.lightning.walletapp.ln.Tools.wrap
-import android.support.v7.widget.Toolbar
 import fr.acinq.bitcoin.Crypto.PublicKey
 import android.os.Bundle
 
+import android.widget.{BaseAdapter, ListView, TextView}
+import android.view.{Menu, View, ViewGroup}
+
 
 class LNStartActivity extends TimerActivity with SearchBar { me =>
-  lazy val toolbar = findViewById(R.id.toolbar).asInstanceOf[Toolbar]
   lazy val lnStartNodesList = findViewById(R.id.lnStartNodesList).asInstanceOf[ListView]
-  lazy val chansNumber = getResources getStringArray R.array.ln_ops_start_node_channels
-  lazy val nodeView = getString(ln_ops_start_node_view)
+  lazy val toolbar = findViewById(R.id.toolbar).asInstanceOf[android.support.v7.widget.Toolbar]
   private var nodes = Vector.empty[StartNodeView]
 
   private val ohHiMarkKey = PublicKey("03dc39d7f43720c2c0f86778dfd2a77049fa4a44b4f0a8afb62f3921567de41375")
@@ -30,25 +28,10 @@ class LNStartActivity extends TimerActivity with SearchBar { me =>
   private val ohHiMark = HardcodedNodeView(app.mkNodeAnnouncement(ohHiMarkKey, "192.210.203.16", 9735), "<i>Bitcoin + Lightning node</i>")
 
   val adapter = new BaseAdapter {
-    def getView(pos: Int, cv: View, parent: ViewGroup) = {
+    def getView(pos: Int, cv: View, par: ViewGroup) = {
       val view = getLayoutInflater.inflate(R.layout.frag_single_line, null)
       val textLine = view.findViewById(R.id.textLine).asInstanceOf[TextView]
-
-      getItem(pos) match {
-        case RemoteNodeView(acn) =>
-          val announce \ connections = acn
-          val humanConnects = app.plurOrZero(chansNumber, connections)
-          val theirNodeKey = humanNode(announce.nodeId.toString, "\u0020")
-          val txt = nodeView.format(announce.alias, humanConnects, theirNodeKey)
-          textLine setText txt.html
-
-        case HardcodedNodeView(announce, tip) =>
-          val theirNodeKey = humanNode(announce.nodeId.toString, "\u0020")
-          val txt = nodeView.format(announce.alias, tip, theirNodeKey)
-          view setBackgroundColor 0xFFEEFFEE
-          textLine setText txt.html
-      }
-
+      textLine setText getItem(pos).asString(nodeView, "\u0020").html
       view
     }
 
@@ -60,7 +43,7 @@ class LNStartActivity extends TimerActivity with SearchBar { me =>
   def INIT(state: Bundle) = if (app.isAlive) {
     new ThrottledWork[String, AnnounceChansNumVec] {
       def work(userQuery: String) = findNodes(userQuery)
-      def error(err: Throwable) = Tools errlog err
+      def error(error: Throwable) = Tools errlog error
       me.react = addWork
 
       def process(ask: String, res: AnnounceChansNumVec) = {
@@ -93,6 +76,28 @@ class LNStartActivity extends TimerActivity with SearchBar { me =>
   }
 }
 
-sealed trait StartNodeView
-case class RemoteNodeView(acn: AnnounceChansNum) extends StartNodeView
-case class HardcodedNodeView(ann: NodeAnnouncement, tip: String) extends StartNodeView
+object StartNodeView {
+  lazy val nodeView = app getString ln_ops_start_node_view
+  lazy val nodeFundView = app getString ln_ops_start_fund_node_view
+  lazy val chansNumber = app.getResources getStringArray R.array.ln_ops_start_node_channels
+}
+
+sealed trait StartNodeView { def asString(base: String, separator: String): String }
+case class HardcodedNodeView(ann: NodeAnnouncement, tip: String) extends StartNodeView {
+
+  def asString(base: String, separator: String) = {
+    val key = humanNode(ann.nodeId.toString, separator)
+    base.format(ann.alias, tip, key)
+  }
+}
+
+// This invariant comes as a search result from Olympus server queries
+case class RemoteNodeView(acn: AnnounceChansNum) extends StartNodeView {
+
+  def asString(base: String, separator: String) = {
+    val channelAnnouncement \ channelConnections = acn
+    val humanConnects = app.plurOrZero(chansNumber, channelConnections)
+    val key = humanNode(channelAnnouncement.nodeId.toString, separator)
+    base.format(channelAnnouncement.alias, humanConnects, key)
+  }
+}
