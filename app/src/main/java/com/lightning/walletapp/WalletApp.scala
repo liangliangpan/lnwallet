b358abfd1dc235066bd9dba06a80109671ecddd5
anton <admin@btcontract.com>
2018-06-19 20:06:02 +0300

    Add neutral buttons

--- app/src/main/java/com/lightning/walletapp/WalletApp.scala
+++ app/src/main/java/com/lightning/walletapp/WalletApp.scala
@@ -95,7 +95,7 @@ class WalletApp extends Application { me =>
     var value: Any = _
     private[this] val prefixes = PaymentRequest.prefixes.values mkString "|"
     private[this] val nodeLink = "([a-fA-F0-9]{66})@([a-zA-Z0-9:\\.\\-_]+):([0-9]+)".r
-    private[this] val lnLink = s"(?im).*?($prefixes)([0-9]{1,}[a-z0-9?]+){1}".r.unanchored
+    private[this] val lnLink = s"(?im).*?($prefixes)([0-9]{1,}[a-z0-9]+){1}".r.unanchored
 
     def recordValue(rawText: String) = value = rawText match {
       case raw if raw startsWith "bitcoin" => new BitcoinURI(params, raw)
