a66a55947a5aaba93bde420c1bfb30727afe9b92
anton <admin@btcontract.com>
2018-07-11 14:38:38 +0300

    [WIP]  external funding

--- app/src/main/java/com/lightning/walletapp/lnutils/ExternalFunder.scala
+++ app/src/main/java/com/lightning/walletapp/lnutils/ExternalFunder.scala
@@ -1,46 +1,72 @@
 package com.lightning.walletapp.lnutils
 
 import spray.json._
+import scala.concurrent.duration._
 import com.neovisionaries.ws.client._
+import com.lightning.walletapp.lnutils.FundMsg._
 import com.lightning.walletapp.lnutils.JsonHttpUtils._
 import com.lightning.walletapp.lnutils.ImplicitJsonFormats._
-import com.lightning.walletapp.ln.Tools.{none, runAnd}
+import com.lightning.walletapp.lnutils.ImplicitConversions._
+import rx.lang.scala.{Observable => Obs}
+import com.lightning.walletapp.ln.Tools
 
 
 object ExternalFunder {
   var worker = Option.empty[Worker]
-  def safeDisconnectWorker: Unit = {
-    for (w <- worker) w.ws.clearListeners
-    for (w <- worker) w.ws.disconnect
-    worker = None
+  def setWorker(newWorker: Worker) = {
+    // This is guaranteed to disconnect it
+    for (old <- worker) disconnectWorker(old)
+    newWorker.ws.connectAsynchronously
+    worker = Some(newWorker)
   }
 
-  def setWorker(parameters: Started) = {
-    if (worker.isDefined) safeDisconnectWorker
-    val newConnection = new Worker(parameters)
-    newConnection.ws.connectAsynchronously
-    worker = Some(newConnection)
-  }
+  def disconnectWorker(oldWorker: Worker) =
+    for (wrk <- worker if wrk.params == oldWorker.params) {
+      // Only disconnect if old worker is also a current worker
+      wrk.ws.clearListeners
+      wrk.ws.disconnect
+      worker = None
+    }
 }
 
-class Worker(parameters: Started) { me =>
-  val ws = (new WebSocketFactory).createSocket(parameters.start.url, 7500)
+case class Worker(params: Started, attemptsLeft: Int = 5) { me =>
+  val url = params.start.url + "?body=" + params.start.toJson.toString.hex
+  val ws: WebSocket = (new WebSocketFactory).createSocket(url, 7500)
   var listeners: Set[ExternalFunderListener] = Set.empty
-  var lastMessage: FundMsg = parameters
+  // Start is uninitialized, Started is server response
+  var lastMessage: FundMsg = params.start
 
-  def update(message: String) = {
-    lastMessage = to[FundMsg](message)
-    for (lst <- listeners) lst onOffline me
+  def shouldReconnect = lastMessage match {
+    case x: Fail => x.code == FAIL_NOT_VERIFIED_YET
+    case _: Fail | _: FundingTxBroadcasted => false
+    case _ => attemptsLeft > 0
   }
 
   ws addListener new WebSocketAdapter {
-    override def onTextMessage(ws: WebSocket, message: String) = try update(message) catch none
-    override def onConnectError(ws: WebSocket, why: WebSocketException) = for (lst <- listeners) lst onOffline me
+    override def onError(ws: WebSocket, why: WebSocketException) = Tools.runAnd(Tools errlog why)(ws.disconnect)
+    override def onTextMessage(ws: WebSocket, message: String) = for (lst <- listeners) lst onMessage to[FundMsg](message)
     override def onDisconnected(ws: WebSocket, scf: WebSocketFrame, ccf: WebSocketFrame, cbs: Boolean) = onConnectError(ws, null)
+
+    override def onConnectError(ws: WebSocket, reason: WebSocketException) = if (shouldReconnect) {
+      Obs.just(null).delay(5.seconds).foreach(_ => for (lst <- listeners) lst onReconnect me, Tools.none)
+      for (listener <- listeners) listener onOffline me
+    } else for (lst <- listeners) lst onCancel me
+  }
+
+  listeners += new ExternalFunderListener {
+    override def onMessage(message: FundMsg) = lastMessage = message
+    override def onCancel(worker: Worker) = ExternalFunder disconnectWorker me
+
+    override def onReconnect(worker: Worker) = {
+      val worker1 = Worker(params, attemptsLeft - 1)
+      ExternalFunder setWorker worker1
+    }
   }
 }
 
 class ExternalFunderListener {
-  def onMessage(message: FundMsg): Unit = none
-  def onOffline(worker: Worker): Unit = none
+  def onMessage(message: FundMsg): Unit = Tools.none
+  def onReconnect(worker: Worker): Unit = Tools.none
+  def onOffline(worker: Worker): Unit = Tools.none
+  def onCancel(worker: Worker): Unit = Tools.none
 }
