2ad43adac14987a21e0f883eb2434c5f1da6f1d2
anton <admin@btcontract.com>
2019-02-22 21:03:41 +0200

    Bump version

--- app/src/main/java/com/lightning/walletapp/SettingsActivity.scala
+++ app/src/main/java/com/lightning/walletapp/SettingsActivity.scala
@@ -112,7 +112,7 @@ class SettingsActivity extends TimerActivity with HumanTimeDisplay { me =>
   def INIT(s: Bundle) = if (app.isAlive) {
     me setContentView R.layout.activity_settings
     me initToolbar findViewById(R.id.toolbar).asInstanceOf[Toolbar]
-    getSupportActionBar setSubtitle "App version 0.3-109"
+    getSupportActionBar setSubtitle "App version 0.3-110"
     getSupportActionBar setTitle wallet_settings
     updateBackupView
 
